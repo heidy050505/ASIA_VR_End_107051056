021cbadc37063e94b7551b6b728788784773d1c1
heidy050505 <heidy050505@gmail.com>
2021-01-12 01:42:51 +0800

    簡陋版發布

---
+++ VR_End_107051056/Assets/scripts/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("丟瓶子");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
